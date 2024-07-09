d1192d227244676d81713522c2b85e20d8664952
rscopim <ricardo.simines@gmail.com>
2024-07-09 12:38:25 -0300

    Lab para monitoramento de rede interna

---
+++ app_monitoramento/go-server/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "net/http"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+    httpRequestsTotal = prometheus.NewCounterVec(
+        prometheus.CounterOpts{
+            Name: "http_requests_total",
+            Help: "Total number of HTTP requests",
+        },
+        []string{"path"},
+    )
+
+    httpDuration = prometheus.NewHistogramVec(
+        prometheus.HistogramOpts{
+            Name:    "http_request_duration_seconds",
+            Help:    "Duration of HTTP requests",
+            Buckets: prometheus.DefBuckets,
+        },
+        []string{"path"},
+    )
+)
+
+func init() {
+    prometheus.MustRegister(httpRequestsTotal)
+    prometheus.MustRegister(httpDuration)
+}
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        timer := prometheus.NewTimer(httpDuration.WithLabelValues(r.URL.Path))
+        defer timer.ObserveDuration()
+
+        httpRequestsTotal.WithLabelValues(r.URL.Path).Inc()
+
+        w.Write([]byte("Hello, World!"))
+    })
+
+    http.Handle("/metrics", promhttp.Handler())
+    http.ListenAndServe(":8080", nil)
+}
